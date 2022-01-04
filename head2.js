2c7435bb373d221cc99006f1153c3a3d17ea89af
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2022-01-04 18:46:05 +0700

    Update head2.js

--- head2.js
+++ head2.js
@@ -1,2 +1,2 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4148964540259192"
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1662098130761985"
      crossorigin="anonymous"></script>
